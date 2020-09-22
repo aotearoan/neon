d5d402136bb0b072e418384ff3868d86a5bdd195
aotearoan <andrew@aotearoan.com>
2020-09-22 22:21:00 +0200

    chip documentation

--- src/components/user-input/chip/NeonChip.ts
+++ src/components/user-input/chip/NeonChip.ts
@@ -7,13 +7,14 @@ import { NeonChipAction } from '../../../common/enums/NeonChipAction';
 
 /**
  * <p>
- * <strong>NeonChip</strong> is a removable chip which can be used as a tag or to indicate selected values in filters
- * and more complex inputs (e.g. search / multiselect). Clicking the chip results in a callback and removal of the chip.
- * Chips are designed to fit inside inputs for creating more complex input components.
+ * <strong>NeonChip</strong> is a clickable or removable chip which can be used as a tag or to indicate selected values
+ * in filters and other more complex inputs (e.g. search / multiselect). Clicking the chip results in a callback and in
+ * the case it is removable, also removal of the chip. Chips are designed to fit inside inputs for creating more complex
+ * input components.
  * </p>
  * <p>
- *   Chips are navigable via tab. When focussed, use space/return/enter to click on clickable tabs and in the case of
- *   removable tabs backspace and delete also remove tabs.
+ *   Chips are navigable via tab (tabindex). When focussed, use space, return or enter to click on clickable tabs and, in
+ *   the case of removable tabs, use backspace and delete to remove a tab. Escape will blur a focussed tab.
  * </p>
  */
 @Component({
