b6f57d8f1c585fc9d2eb19c6ff8c36eb4bc29bae
aotearoan <andrew@aotearoan.com>
2020-10-29 20:15:28 +0100

    Table style docs, anchor refactoring

--- src/app/components/component-documentation/ComponentDocumentation.vue
+++ src/app/components/component-documentation/ComponentDocumentation.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="component-documentation">
+  <div class="component-documentation" v-if="selected && tabs.length > 0">
     <div class="component-documentation__header">
       <div class="component-documentation__header-container">
         <h1 class="component-documentation__title neon-h2">{{ componentTitle }}</h1>
@@ -7,7 +7,12 @@
         <neon-tabs :tabs="tabs" v-model="selected" @input="onChangeTab" :underline="false" />
       </div>
     </div>
-    <neon-tab :tab="tabs[examplesIndex]" :selected="selected === tabs[examplesIndex].key" :toggleOnIf="true">
+    <neon-tab
+      v-if="examplesIndex >= 0"
+      :tab="tabs[examplesIndex]"
+      :selected="selected === tabs[examplesIndex].key"
+      :toggleOnIf="true"
+    >
       <h2 v-if="descriptionIndex === -1" class="neon-h3 component-documentation__description-heading">Description</h2>
       <slot v-if="descriptionIndex === -1"></slot>
       <examples :examples="examples" />
@@ -21,7 +26,7 @@
       <h2 v-if="$slots.default" class="neon-h3">Description</h2>
       <slot></slot>
     </neon-tab>
-    <neon-tab :tab="tabs[apiIndex]" :selected="selected === tabs[apiIndex].key" :toggleOnIf="true">
+    <neon-tab v-if="apiIndex >= 0" :tab="tabs[apiIndex]" :selected="selected === tabs[apiIndex].key" :toggleOnIf="true">
       <h3 class="component-documentation__tab-title">API</h3>
       <api-docs v-if="apiModel" :api-model="apiModel" :component-name="componentName" />
       <api-docs
---
+++ src/app/views/presentation/table/Table.vue
@@ -0,0 +1,11 @@
+<template>
+  <component-documentation v-if="menuModel" :examples="examples" :model="menuModel" :headline="headline">
+    <neon-note color="warn">
+      <span>
+        <strong>Note:</strong> There is no table component in Neon. However, Neon does provide styling for tables.
+      </span>
+    </neon-note>
+  </component-documentation>
+</template>
+
+<script lang="ts" src="./Table.ts"></script>
