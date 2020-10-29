5ae7152dfb930cf63a9e8c2550513ca7afb8f7d9
aotearoan <andrew@aotearoan.com>
2020-10-29 18:30:46 +0100

    application structure adjustments

--- src/components/layout/page/NeonPage.vue
+++ src/components/layout/page/NeonPage.vue
@@ -3,14 +3,14 @@
     class="neon-page"
     :class="{ 'neon-page--with-top-nav': $slots['top-nav'], 'neon-page--with-side-nav': $slots['side-nav'] }"
   >
-    <div class="neon-page__container">
-      <!-- @slot The main page content -->
-      <slot name="content"></slot>
-    </div>
     <!-- @slot The <strong>NeonTopNav</strong> slot. This slot is required to ensure the correct responsive page layout. -->
     <slot name="top-nav"></slot>
     <!-- @slot The <strong>NeonSideNav</strong> slot. This slot is required to ensure the correct responsive page layout. -->
     <slot name="side-nav"></slot>
+    <div class="neon-page__container">
+      <!-- @slot The main page content -->
+      <slot name="content"></slot>
+    </div>
   </div>
 </template>
 
