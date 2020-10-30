7a2d722a07964a29a0ecfa77bbc1a871332bae63
aotearoan <andrew@aotearoan.com>
2020-10-30 01:15:29 +0100

    Modal accessibility

--- src/components/layout/modal/NeonModal.vue
+++ src/components/layout/modal/NeonModal.vue
@@ -1,13 +1,16 @@
 <template>
-  <div class="neon-modal" :class="{ 'neon-modal--open': open }">
+  <div class="neon-modal" :class="{ 'neon-modal--open': open }" role="dialog">
     <div class="neon-modal__container" ref="modal">
       <!-- @slot modal contents -->
       <slot></slot>
       <neon-button
-        @click="close()"
+        @click="close"
+        @keydown.space="$event.stopPropagation() && close() && $event.preventDefault()"
+        @keydown.enter="close"
         button-style="text"
         icon="times"
         size="s"
+        tabindex="0"
         :circular="true"
         class="neon-modal__close"
         color="low-contrast"
