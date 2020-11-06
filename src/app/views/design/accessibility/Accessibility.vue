a9ebbb649f3e3ab9432b0b5fbd194a2214ebcc8b
aotearoan <andrew@aotearoan.com>
2020-11-06 22:41:03 +0100

    Accessibility page

---
+++ src/app/views/design/accessibility/Accessibility.vue
@@ -0,0 +1,20 @@
+<template>
+  <neon-card>
+    <neon-card-header>
+      <h1 class="neon-h3">Accessibility</h1>
+    </neon-card-header>
+    <neon-card-body>
+      <p>
+        As much as possible Neon tries to use semantic HTML for creating components. This means most interactive
+        components are implicitly accessible. Where semantic HTML is either not possible or unavailable Neon supports
+        ARIA keyboard accessibility as well as the relevant ARIA roles and attributes on each component.
+      </p>
+      <p>
+        Please see individual component examples and APIs to see how they behave. Also the source code in GitHub can
+        provide more about specific implementation details.
+      </p>
+    </neon-card-body>
+  </neon-card>
+</template>
+
+<script lang="ts" src="./Accessibility.ts"></script>
