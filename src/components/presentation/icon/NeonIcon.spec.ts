febd460e3fd63c0aa75b7de8bfc5632fd854b1f0
Aotearoan <andrew@aotearoan.com>
2020-12-22 15:36:27 +0100

    icon tests (#81)

--- src/components/presentation/icon/NeonIcon.spec.ts
+++ src/components/presentation/icon/NeonIcon.spec.ts
@@ -13,12 +13,33 @@ describe('NeonIcon', () => {
     expect(wrapper.find(`.neon-icon--${color}`).element).toBeDefined();
   });
 
+  it('renders inverse', () => {
+    const wrapper = shallowMount(NeonIcon, {
+      propsData: { name, inverse: true },
+    });
+    expect(wrapper.find('.neon-icon--inverse').element).toBeDefined();
+  });
+
+  it('renders disabled', () => {
+    const wrapper = shallowMount(NeonIcon, {
+      propsData: { name, disabled: true },
+    });
+    expect(wrapper.find('.neon-icon--disabled').element).toBeDefined();
+  });
+
   it('gets icon', () => {
-    const color = NeonFunctionalColor.Warn;
     // eslint-disable-next-line @typescript-eslint/no-explicit-any
     const wrapper: any = shallowMount(NeonIcon, {
-      propsData: { name, color },
+      propsData: { name },
     });
     expect(wrapper.vm.icon).toBeDefined();
+  });
+
+  it('does not render missing icon', () => {
+    // eslint-disable-next-line @typescript-eslint/no-explicit-any
+    const wrapper: any = shallowMount(NeonIcon, {
+      propsData: { name: 'xdd' },
+    });
+    expect(wrapper.vm.icon).toBeUndefined();
   });
 });
