bd537c4144dbb335d20da5e99811ffcb021e695c
aotearoan <andrew@aotearoan.com>
2020-12-23 17:49:22 +0100

    link tests/imports

--- src/components/navigation/link/NeonLink.spec.ts
+++ src/components/navigation/link/NeonLink.spec.ts
@@ -1,5 +1,10 @@
 import { shallowMount, RouterLinkStub } from '@vue/test-utils';
 import NeonLink from './NeonLink.vue';
+import Vue from 'vue';
+import NeonIcon from '../../presentation/icon/NeonIcon.vue';
+import { NeonOutlineStyle } from '../../../common/enums/NeonOutlineStyle';
+
+Vue.component('NeonIcon', NeonIcon);
 
 describe('NeonLink', () => {
   it('renders default slot contents external link', () => {
@@ -36,7 +41,21 @@ describe('NeonLink', () => {
     expect(wrapper.find('.neon-link p').text()).toEqual(slotValue);
   });
 
-  it('renders external link', () => {
+  it('renders external link with indicator', () => {
+    // given
+    const href = 'http://www.getskeleton.com';
+    const wrapper = shallowMount(NeonLink, {
+      propsData: { href, externalIndicator: true },
+      stubs: {
+        RouterLink: RouterLinkStub,
+      },
+    });
+    // when / then
+    expect(wrapper.find('.neon-link--external-link').element).toBeDefined();
+    expect(wrapper.find('.neon-link--with-external-indicator').element).toBeDefined();
+  });
+
+  it('renders external link without indicator', () => {
     // given
     const href = 'http://www.getskeleton.com';
     const wrapper = shallowMount(NeonLink, {
@@ -47,9 +66,10 @@ describe('NeonLink', () => {
     });
     // when / then
     expect(wrapper.find('.neon-link--external-link').element).toBeDefined();
+    expect(wrapper.find('.neon-link--with-external-indicator').element).toBeUndefined();
   });
 
-  it('renders router link link', () => {
+  it('renders router link', () => {
     // given
     const href = '/xd';
     const wrapper = shallowMount(NeonLink, {
@@ -73,5 +93,59 @@ describe('NeonLink', () => {
     });
     // when / then
     expect(wrapper.find('.neon-link--no-style').element).toBeDefined();
+  });
+
+  it('renders default outline style', () => {
+    // given
+    const href = '/xd';
+    const wrapper = shallowMount(NeonLink, {
+      propsData: { href },
+      stubs: {
+        RouterLink: RouterLinkStub,
+      },
+    });
+    // when / then
+    expect(wrapper.find('.neon-link--outline-text').element).toBeDefined();
+  });
+
+  it('renders provided outline style', () => {
+    // given
+    const href = '/xd';
+    const wrapper = shallowMount(NeonLink, {
+      propsData: { href, outlineStyle: NeonOutlineStyle.Border },
+      stubs: {
+        RouterLink: RouterLinkStub,
+      },
+    });
+    // when / then
+    expect(wrapper.find('.neon-link--outline-border').element).toBeDefined();
+  });
+
+  it('emits click event', () => {
+    // given
+    const wrapper = shallowMount(NeonLink, {
+      propsData: {},
+      stubs: {
+        RouterLink: RouterLinkStub,
+      },
+    });
+    // when
+    wrapper.find('.neon-link').trigger('click');
+    // then
+    expect(wrapper.emitted().click[0]).toBeDefined();
+  });
+
+  it('emits click event on enter keydown', () => {
+    // given
+    const wrapper = shallowMount(NeonLink, {
+      propsData: {},
+      stubs: {
+        RouterLink: RouterLinkStub,
+      },
+    });
+    // when
+    wrapper.find('.neon-link').trigger('keydown.enter');
+    // then
+    expect(wrapper.emitted().click[0]).toBeDefined();
   });
 });
