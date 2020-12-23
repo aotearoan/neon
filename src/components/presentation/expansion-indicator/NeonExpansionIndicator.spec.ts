ee7d9b2cea3935e06c1ef6e5e7490549b5d1f609
aotearoan <andrew@aotearoan.com>
2020-12-23 18:36:45 +0100

    expansion indicator tests

--- src/components/presentation/expansion-indicator/NeonExpansionIndicator.spec.ts
+++ src/components/presentation/expansion-indicator/NeonExpansionIndicator.spec.ts
@@ -1,5 +1,6 @@
 import { shallowMount } from '@vue/test-utils';
 import NeonExpansionIndicator from './NeonExpansionIndicator.vue';
+import { NeonFunctionalColor } from '../../../common/enums/NeonFunctionalColor';
 
 describe('NeonExpansionIndicator', () => {
   it('renders indicator closed', () => {
@@ -18,5 +19,32 @@ describe('NeonExpansionIndicator', () => {
     });
     // when / then
     expect(wrapper.find('.neon-expansion-indicator--expanded').element).toBeDefined();
+  });
+
+  it('renders inverse', () => {
+    // given
+    const wrapper = shallowMount(NeonExpansionIndicator, {
+      propsData: { inverse: true },
+    });
+    // when / then
+    expect(wrapper.find('.neon-expansion-indicator--inverse').element).toBeDefined();
+  });
+
+  it('renders disabled', () => {
+    // given
+    const wrapper = shallowMount(NeonExpansionIndicator, {
+      propsData: { disabled: true },
+    });
+    // when / then
+    expect(wrapper.find('.neon-expansion-indicator--disabled').element).toBeDefined();
+  });
+
+  it('renders indicator color', () => {
+    // given
+    const wrapper = shallowMount(NeonExpansionIndicator, {
+      propsData: { color: NeonFunctionalColor.Info },
+    });
+    // when / then
+    expect(wrapper.find('.neon-expansion-indicator--info').element).toBeDefined();
   });
 });
