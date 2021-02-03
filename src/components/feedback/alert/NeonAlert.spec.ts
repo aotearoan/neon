c06164a631c3a4b14935fd9fb6c108142961a4fc
aotearoan <andrew@aotearoan.com>
2021-02-03 01:37:44 +0100

    complete alert component tests

--- src/components/feedback/alert/NeonAlert.spec.ts
+++ src/components/feedback/alert/NeonAlert.spec.ts
@@ -1,6 +1,7 @@
 import Vue from 'vue';
 import { mount } from '@vue/test-utils';
 import NeonAlert from './NeonAlert.vue';
+import NeonAlertClass from './NeonAlert';
 import NeonAlertContainer from './container/NeonAlertContainer.vue';
 import NeonToastContainer from './container/NeonToastContainer.vue';
 import { NeonAlertService } from '../../../common/utils/NeonAlertService';
@@ -8,6 +9,7 @@ import { NeonToastService } from '../../../common/utils/NeonToastService';
 import { NeonAlertPlacement } from '../../../common/enums/NeonAlertPlacement';
 import { NeonVerticalPosition } from '../../../common/enums/NeonVerticalPosition';
 import { NeonEventBus } from '../../../common/utils/NeonEventBus';
+import { NeonAlertLevel } from '../../../common/enums/NeonAlertLevel';
 
 Vue.component('NeonAlertContainer', NeonAlertContainer);
 Vue.component('NeonToastContainer', NeonToastContainer);
@@ -237,10 +239,38 @@ describe('NeonAlert', () => {
     });
   });
 
+  it('renders message duration > 0', (done) => {
+    // given
+    const wrapper = mount(NeonAlert, {
+      propsData: {},
+    });
+    // when
+    NeonAlertService.info({ title: 'test', duration: 1 });
+    // then
+    setTimeout(() => {
+      expect(wrapper.find('.neon-alert__message--info').element).toBeDefined();
+      done();
+    });
+  });
+
   it('renders toast duration 0', (done) => {
     // given
     const wrapper = mount(NeonAlert, {
       propsData: { duration: 0 },
+    });
+    // when
+    NeonToastService.info({ title: 'test' });
+    // then
+    setTimeout(() => {
+      expect(wrapper.find('.neon-toast__message--info').element).toBeDefined();
+      done();
+    });
+  });
+
+  it('renders toast duration > 0', (done) => {
+    // given
+    const wrapper = mount(NeonAlert, {
+      propsData: { duration: 1 },
     });
     // when
     NeonToastService.info({ title: 'test' });
@@ -263,6 +293,114 @@ describe('NeonAlert', () => {
       expect(wrapper.find('.neon-toast__message--info').element).toBeDefined();
       done();
     });
+  });
+
+  it('removes toast top', () => {
+    // given
+    const wrapper = mount(NeonAlert, {
+      propsData: {},
+    });
+    const component = wrapper.vm as NeonAlertClass;
+    // @ts-ignore
+    component.top = [{ id: 0 }];
+    // when
+    component.removeToast({ id: 0, level: NeonAlertLevel.Info, title: 'test', duration: 50 });
+    // then
+    expect(component.top.length).toEqual(0);
+  });
+
+  it('removes toast bottom', () => {
+    // given
+    const wrapper = mount(NeonAlert, {
+      propsData: {},
+    });
+    const component = wrapper.vm as NeonAlertClass;
+    // @ts-ignore
+    component.bottom = [{ id: 0 }];
+    // when
+    component.removeToast({
+      id: 0,
+      level: NeonAlertLevel.Info,
+      title: 'test',
+      duration: 50,
+      placement: NeonVerticalPosition.Bottom,
+    });
+    // then
+    expect(component.bottom.length).toEqual(0);
+  });
+
+  it('removes alert top right', () => {
+    // given
+    const wrapper = mount(NeonAlert, {
+      propsData: {},
+    });
+    const component = wrapper.vm as NeonAlertClass;
+    // @ts-ignore
+    component.topRight = [{ id: 0 }];
+    // when
+    component.removeAlert({ id: 0, level: NeonAlertLevel.Info, title: 'test', duration: 50 });
+    // then
+    expect(component.topRight.length).toEqual(0);
+  });
+
+  it('removes alert top left', () => {
+    // given
+    const wrapper = mount(NeonAlert, {
+      propsData: {},
+    });
+    const component = wrapper.vm as NeonAlertClass;
+    // @ts-ignore
+    component.topLeft = [{ id: 0 }];
+    // when
+    component.removeAlert({
+      id: 0,
+      level: NeonAlertLevel.Info,
+      title: 'test',
+      duration: 50,
+      placement: NeonAlertPlacement.TopLeft,
+    });
+    // then
+    expect(component.topLeft.length).toEqual(0);
+  });
+
+  it('removes alert bottom left', () => {
+    // given
+    const wrapper = mount(NeonAlert, {
+      propsData: {},
+    });
+    const component = wrapper.vm as NeonAlertClass;
+    // @ts-ignore
+    component.bottomLeft = [{ id: 0 }];
+    // when
+    component.removeAlert({
+      id: 0,
+      level: NeonAlertLevel.Info,
+      title: 'test',
+      duration: 50,
+      placement: NeonAlertPlacement.BottomLeft,
+    });
+    // then
+    expect(component.bottomLeft.length).toEqual(0);
+  });
+
+  it('removes alert bottom right', () => {
+    // given
+    const wrapper = mount(NeonAlert, {
+      propsData: {},
+    });
+    const component = wrapper.vm as NeonAlertClass;
+    // @ts-ignore
+    component.bottomRight = [{ id: 0 }];
+    // when
+    component.removeAlert({
+      id: 0,
+      level: NeonAlertLevel.Info,
+      title: 'test',
+      duration: 50,
+      placement: NeonAlertPlacement.BottomRight,
+    });
+    // then
+    expect(component.bottomRight.length).toEqual(0);
   });
 
   it('removes listeners on destroy', () => {
