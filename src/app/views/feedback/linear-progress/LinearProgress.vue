cbc2196a6359db83e114a12321cd44a6f40d02f7
Aotearoan <andrew@aotearoan.com>
2022-07-16 18:59:44 +0200

    add linear progress component (#165)

---
+++ src/app/views/feedback/linear-progress/LinearProgress.vue
@@ -0,0 +1,14 @@
+<template>
+  <component-documentation v-if="menuModel" :examples="examples" :model="menuModel" :headline="headline">
+    <neon-card>
+      <neon-card-body>
+        <p>
+          A component for displaying linear progress. The progress can be displayed as a percentage (default) or as a
+          counter (x / y).
+        </p>
+      </neon-card-body>
+    </neon-card>
+  </component-documentation>
+</template>
+
+<script lang="ts" src="./LinearProgress.ts"></script>
