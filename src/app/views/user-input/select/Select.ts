9ec57aa3954a48f02ade11a2e92cb041d3560626
aotearoan <andrew@aotearoan.com>
2023-04-13 17:24:11 +0200

    fix(docs): Select docs

--- src/app/views/user-input/select/Select.ts
+++ src/app/views/user-input/select/Select.ts
@@ -18,259 +18,238 @@ export default defineComponent({
     const menuModel = ref<MenuModel | null>(null);
     const headline = ref('Styled HTML select equivalent');
 
-    const data = ref({
-      selectSmall: '',
-      selectMedium: '',
-      selectLarge: '',
-      selectMulti: [],
-      selectGrouped: '',
-      selectGroupedMulti: [],
-      selectBrand: '',
-      selectInfo: '',
-      selectSuccess: '',
-      selectWarning: '',
-      selectError: '',
-      model: [
-        {
-          key: 'k1',
-          label: 'Item 1',
-          separatorBefore: true,
-        },
-        {
-          key: 'k2',
-          label: 'Item 2',
-          separatorBefore: true,
-        },
-        {
-          key: 'k3',
-          label: 'Item 3',
-          separatorBefore: true,
-        },
-        {
-          key: 'k4',
-          label: 'Item 4',
-          separatorBefore: true,
-        },
-        {
-          key: 'k5',
-          label: 'Item 5',
-          separatorBefore: true,
-        },
-        {
-          key: 'k6',
-          label: 'Item 6',
-          disabled: true,
-          separatorBefore: true,
-        },
-      ],
-      modelWithIcons: [
-        {
-          key: 'k1',
-          label: 'Item 1',
-          icon: 'contrast',
-          separatorBefore: true,
-        },
-        {
-          key: 'k2',
-          label: 'Item 2',
-          icon: 'user',
-          separatorBefore: true,
-        },
-        {
-          key: 'k3',
-          label: 'Item 3',
-          icon: 'plus',
-          separatorBefore: true,
-        },
-        {
-          key: 'k4',
-          label: 'Item 4',
-          icon: 'download',
-          separatorBefore: true,
-        },
-        {
-          key: 'k5',
-          label: 'Item 5',
-          icon: 'hammer',
-          separatorBefore: true,
-        },
-        {
-          key: 'k6',
-          label: 'Item 6',
-          icon: 'images',
-          disabled: true,
-          separatorBefore: true,
-        },
-      ],
-      groupedModel: [
-        {
-          group: 'Africa',
-          options: [
-            {
-              key: 'AGO',
-              label: 'Angola',
-            },
-            {
-              key: 'BFA',
-              label: 'Burkina Faso',
-            },
-            {
-              key: 'KEN',
-              label: 'Kenya',
-            },
-            {
-              key: 'MLI',
-              label: 'Mali',
-            },
-          ],
-        },
-        {
-          group: 'America',
-          options: [
-            {
-              key: 'CAN',
-              label: 'Canada',
-            },
-            {
-              key: 'PAN',
-              label: 'Panama',
-            },
-            {
-              key: 'USA',
-              label: 'United States',
-            },
-            {
-              key: 'VEN',
-              label: 'Venezuela',
-            },
-          ],
-        },
-        {
-          group: 'Asia',
-          options: [
-            {
-              key: 'CHN',
-              label: 'China',
-            },
-            {
-              key: 'PHL',
-              label: 'Philippines',
-            },
-            {
-              key: 'SNG',
-              label: 'Singapore',
-            },
-            {
-              key: 'THA',
-              label: 'Thailand',
-            },
-          ],
-        },
-        {
-          group: 'Europe',
-          options: [
-            {
-              key: 'BEL',
-              label: 'Belgium',
-            },
-            {
-              key: 'FRA',
-              label: 'France',
-            },
-            {
-              key: 'DEU',
-              label: 'Germany',
-            },
-            {
-              key: 'NOR',
-              label: 'Norway',
-            },
-          ],
-        },
-        {
-          group: 'Oceania',
-          options: [
-            {
-              key: 'AUS',
-              label: 'Australia',
-            },
-            {
-              key: 'FJI',
-              label: 'Fiji',
-            },
-            {
-              key: 'NZL',
-              label: 'New Zealand',
-            },
-            {
-              key: 'TKL',
-              label: 'Tokelau',
-            },
-          ],
-        },
-      ],
-    });
+    const selectSmall = ref('');
+    const selectMedium = ref('');
+    const selectLarge = ref('');
+    const selectMulti = ref([]);
+    const selectGrouped = ref('');
+    const selectGroupedMulti = ref([]);
+    const selectBrand = ref('');
+    const selectInfo = ref('');
+    const selectSuccess = ref('');
+    const selectWarning = ref('');
+    const selectError = ref('');
 
-    const examples = ref([
+    const model = ref([
       {
-        title: 'Select sizes',
-        template: `
-          <div class="neon-vertically-spaced">
-          <neon-select size="s" placeholder="Select item" :options="model" :model-value="selectSmall"
-                       @update:modelValue="updateSelection('selectSmall', $event)" />
-          <neon-select size="m" placeholder="Select item" :options="model" :model-value="selectMedium"
-                       @update:modelValue="updateSelection('selectMedium', $event)" />
-          <neon-select size="l" placeholder="Select item" :options="model" :model-value="selectLarge"
-                       @update:modelValue="updateSelection('selectLarge', $event)" />
-          </div>`,
-        data,
+        key: 'k1',
+        label: 'Item 1',
+        separatorBefore: true,
       },
       {
-        title: 'Selects with colors and icons',
-        template: `
-          <div class="neon-vertically-spaced">
-          <neon-select color="brand" placeholder="Select item(s)" :options="modelWithIcons" :model-value="selectBrand"
-                       @update:modelValue="updateSelection('select', $event)" />
-          <neon-select color="info" placeholder="Select item(s)" :options="modelWithIcons" :model-value="selectInfo"
-                       @update:modelValue="updateSelection('select', $event)" />
-          <neon-select color="success" placeholder="Select item(s)" :options="modelWithIcons"
-                       :model-value="selectSuccess" @update:modelValue="updateSelection('select', $event)" />
-          <neon-select color="warn" placeholder="Select item(s)" :options="modelWithIcons" :model-value="selectWarning"
-                       @update:modelValue="updateSelection('select', $event)" />
-          <neon-select color="error" placeholder="Select item(s)" :options="modelWithIcons" :model-value="selectError"
-                       @update:modelValue="updateSelection('select', $event)" />
-          </div>`,
-        data,
+        key: 'k2',
+        label: 'Item 2',
+        separatorBefore: true,
       },
       {
-        title: 'Multiple selection',
-        template: `
-          <div class="neon-vertically-spaced">
-          <neon-select :multiple="true" placeholder="Select item(s)" :options="modelWithIcons"
-                       :model-value="selectMulti" @update:modelValue="updateSelection('select', $event)" />
-          </div>`,
-        data,
+        key: 'k3',
+        label: 'Item 3',
+        separatorBefore: true,
       },
       {
-        title: 'Grouped options',
-        template: `
-          <div class="neon-vertically-spaced">
-          <neon-select placeholder="Select item" :grouped-options="groupedModel" :model-value="selectGrouped"
-                       @update:modelValue="updateSelection('select', $event)" />
-          <neon-select :multiple="true" placeholder="Select item(s)" :grouped-options="groupedModel"
-                       :model-value="selectGroupedMulti" @update:modelValue="updateSelection('select', $event)" />
-          </div>`,
-        data,
+        key: 'k4',
+        label: 'Item 4',
+        separatorBefore: true,
+      },
+      {
+        key: 'k5',
+        label: 'Item 5',
+        separatorBefore: true,
+      },
+      {
+        key: 'k6',
+        label: 'Item 6',
+        disabled: true,
+        separatorBefore: true,
       },
     ]);
+
+    const modelWithIcons = ([
+      {
+        key: 'k1',
+        label: 'Item 1',
+        icon: 'contrast',
+        separatorBefore: true,
+      },
+      {
+        key: 'k2',
+        label: 'Item 2',
+        icon: 'user',
+        separatorBefore: true,
+      },
+      {
+        key: 'k3',
+        label: 'Item 3',
+        icon: 'plus',
+        separatorBefore: true,
+      },
+      {
+        key: 'k4',
+        label: 'Item 4',
+        icon: 'download',
+        separatorBefore: true,
+      },
+      {
+        key: 'k5',
+        label: 'Item 5',
+        icon: 'hammer',
+        separatorBefore: true,
+      },
+      {
+        key: 'k6',
+        label: 'Item 6',
+        icon: 'images',
+        disabled: true,
+        separatorBefore: true,
+      },
+    ]);
+
+    const groupedModel = ([
+      {
+        group: 'Africa',
+        options: [
+          {
+            key: 'AGO',
+            label: 'Angola',
+          },
+          {
+            key: 'BFA',
+            label: 'Burkina Faso',
+          },
+          {
+            key: 'KEN',
+            label: 'Kenya',
+          },
+          {
+            key: 'MLI',
+            label: 'Mali',
+          },
+        ],
+      },
+      {
+        group: 'America',
+        options: [
+          {
+            key: 'CAN',
+            label: 'Canada',
+          },
+          {
+            key: 'PAN',
+            label: 'Panama',
+          },
+          {
+            key: 'USA',
+            label: 'United States',
+          },
+          {
+            key: 'VEN',
+            label: 'Venezuela',
+          },
+        ],
+      },
+      {
+        group: 'Asia',
+        options: [
+          {
+            key: 'CHN',
+            label: 'China',
+          },
+          {
+            key: 'PHL',
+            label: 'Philippines',
+          },
+          {
+            key: 'SNG',
+            label: 'Singapore',
+          },
+          {
+            key: 'THA',
+            label: 'Thailand',
+          },
+        ],
+      },
+      {
+        group: 'Europe',
+        options: [
+          {
+            key: 'BEL',
+            label: 'Belgium',
+          },
+          {
+            key: 'FRA',
+            label: 'France',
+          },
+          {
+            key: 'DEU',
+            label: 'Germany',
+          },
+          {
+            key: 'NOR',
+            label: 'Norway',
+          },
+        ],
+      },
+      {
+        group: 'Oceania',
+        options: [
+          {
+            key: 'AUS',
+            label: 'Australia',
+          },
+          {
+            key: 'FJI',
+            label: 'Fiji',
+          },
+          {
+            key: 'NZL',
+            label: 'New Zealand',
+          },
+          {
+            key: 'TKL',
+            label: 'Tokelau',
+          },
+        ],
+      },
+    ]);
+
+
+    const sizesTemplate = `<neon-select v-model="selectSmall" :options="model" placeholder="Select item" size="s" />
+<neon-select v-model="selectMedium" :options="model" placeholder="Select item" size="m" />
+<neon-select v-model="selectLarge" :options="model" placeholder="Select item" size="l" />`;
+
+    const colorsTemplate = `<neon-select v-model="selectBrand" :options="modelWithIcons" color="brand" placeholder="Select item(s)" />
+<neon-select v-model="selectInfo" :options="modelWithIcons" color="info" placeholder="Select item(s)" />
+<neon-select v-model="selectSuccess" :options="modelWithIcons" color="success" placeholder="Select item(s)" />
+<neon-select v-model="selectWarning" :options="modelWithIcons" color="warn" placeholder="Select item(s)" />
+<neon-select v-model="selectError" :options="modelWithIcons" color="error" placeholder="Select item(s)" />`;
+
+    const multiSelectTemplate = `<neon-select v-model="selectMulti" :multiple="true" :options="modelWithIcons" placeholder="Select item(s)" />`;
+    const groupedTemplate = `<neon-select v-model="selectGrouped" :grouped-options="groupedModel" placeholder="Select item" />
+<neon-select v-model="selectGroupedMulti" :grouped-options="groupedModel" :multiple="true" placeholder="Select item(s)" />`;
 
     onMounted(() => (menuModel.value = Menu.getComponentConfig('NeonSelect')));
 
     return {
       menuModel,
       headline,
-      data,
-      examples,
+      selectSmall,
+      selectMedium,
+      selectLarge,
+      selectMulti,
+      selectGrouped,
+      selectGroupedMulti,
+      selectBrand,
+      selectInfo,
+      selectSuccess,
+      selectWarning,
+      selectError,
+      model,
+      modelWithIcons,
+      groupedModel,
+      sizesTemplate,
+      colorsTemplate,
+      multiSelectTemplate,
+      groupedTemplate,
     };
   },
 });
--- src/app/views/user-input/select/Select.vue
+++ src/app/views/user-input/select/Select.vue
@@ -1,9 +1,8 @@
 <template>
   <component-documentation
     v-if="menuModel"
-    :examples="examples"
-    :model="menuModel"
     :headline="headline"
+    :model="menuModel"
     class="select-examples"
   >
     <neon-card>
@@ -19,8 +18,41 @@
         </p>
         <p>
           <strong>Note:</strong> As well as the options described below, pass through attributes supported by
-          <neon-link href="/presentation/dropdown">NeonDropdown</neon-link> to change the style of the dropdown button.
+          <neon-link href="/presentation/dropdown">NeonDropdown</neon-link>
+          to change the style of the dropdown button.
         </p>
+      </neon-card-body>
+      <neon-card-body>
+        <h2 class="neon-h3">Select sizes</h2>
+        <div class="neon-vertically-spaced">
+          <neon-select v-model="selectSmall" :options="model" placeholder="Select item" size="s" />
+          <neon-select v-model="selectMedium" :options="model" placeholder="Select item" size="m" />
+          <neon-select v-model="selectLarge" :options="model" placeholder="Select item" size="l" />
+        </div>
+      </neon-card-body>
+      <neon-card-body>
+        <h2 class="neon-h3">Selects with colors and icons</h2>
+        <div class="neon-vertically-spaced">
+          <neon-select v-model="selectBrand" :options="modelWithIcons" color="brand" placeholder="Select item(s)" />
+          <neon-select v-model="selectInfo" :options="modelWithIcons" color="info" placeholder="Select item(s)" />
+          <neon-select v-model="selectSuccess" :options="modelWithIcons" color="success" placeholder="Select item(s)" />
+          <neon-select v-model="selectWarning" :options="modelWithIcons" color="warn" placeholder="Select item(s)" />
+          <neon-select v-model="selectError" :options="modelWithIcons" color="error" placeholder="Select item(s)" />
+        </div>
+      </neon-card-body>
+      <neon-card-body>
+        <h2 class="neon-h3">Multiple selection</h2>
+        <div class="neon-vertically-spaced">
+          <neon-select v-model="selectMulti" :multiple="true" :options="modelWithIcons" placeholder="Select item(s)" />
+        </div>
+      </neon-card-body>
+      <neon-card-body>
+        <h2 class="neon-h3">Grouped options</h2>
+        <div class="neon-vertically-spaced">
+          <neon-select v-model="selectGrouped" :grouped-options="groupedModel" placeholder="Select item" />
+          <neon-select v-model="selectGroupedMulti" :grouped-options="groupedModel" :multiple="true"
+                       placeholder="Select item(s)" />
+        </div>
       </neon-card-body>
     </neon-card>
   </component-documentation>
