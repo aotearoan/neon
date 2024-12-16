af107b0cec98042277e78d710f072d7a67cfccc2
Aotearoan <andrew@aotearoan.com>
2024-12-16 21:15:01 +0100

    fix: tree menu bugfixes (#199)
    * fix: tree menu bugfixes
    
    * fix: debounce timeout type
    
    * fix: date tests

--- public/files/models/NeonTreeMenuSectionModel.ts
+++ public/files/models/NeonTreeMenuSectionModel.ts
@@ -3,6 +3,7 @@ import type { NeonTreeMenuLinkModel } from './NeonTreeMenuLinkModel';
 export interface NeonTreeMenuSectionModel {
   label: string;
   key: string;
+  href?: string;
   children?: NeonTreeMenuLinkModel[];
   expanded?: boolean;
   disabled?: boolean;
