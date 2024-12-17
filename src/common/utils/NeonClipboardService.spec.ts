1346fd7ed56dabce2a22df91113257fa0616e482
aotearoan <andrew@aotearoan.com>
2024-12-17 22:16:15 +0100

    fix: tests

--- src/common/utils/NeonClipboardService.spec.ts
+++ src/common/utils/NeonClipboardService.spec.ts
@@ -1,29 +1,9 @@
 import { NeonClipboardSupport } from './NeonClipboardService';
 
 describe('NeonClipboardService', () => {
-  const clipboard = navigator.clipboard;
-  const permissions = navigator.permissions;
-
-  afterEach(() => {
-    Object.defineProperty(navigator, 'clipboard', {
-      value: clipboard,
-      writable: true,
-    });
-
-    Object.defineProperty(navigator, 'permissions', {
-      value: permissions,
-      writable: true,
-    });
-  });
-
-  it('copies to clipboard when supported & granted', (done) => {
+  it('copies to clipboard', (done) => {
     Object.defineProperty(navigator, 'clipboard', {
       value: { writeText: jest.fn() },
-      writable: true,
-    });
-
-    Object.defineProperty(navigator, 'permissions', {
-      value: { query: () => Promise.resolve({ state: 'granted' }) },
       writable: true,
     });
 
@@ -33,47 +13,6 @@ describe('NeonClipboardService', () => {
     setTimeout(async () => {
       await clippy.copyTo(toCopy);
       expect(navigator.clipboard.writeText).toHaveBeenCalledWith(toCopy);
-      done();
-    });
-  });
-
-  it('copies to clipboard when supported & prompt', (done) => {
-    Object.defineProperty(navigator, 'clipboard', {
-      value: { writeText: jest.fn() },
-      writable: true,
-    });
-
-    Object.defineProperty(navigator, 'permissions', {
-      value: { query: () => Promise.resolve({ state: 'prompt' }) },
-      writable: true,
-    });
-
-    const toCopy = 'xdd';
-    const clippy = new NeonClipboardSupport();
-
-    setTimeout(async () => {
-      await clippy.copyTo(toCopy);
-      expect(navigator.clipboard.writeText).toHaveBeenCalledWith(toCopy);
-      done();
-    });
-  });
-
-  it('does not copy when unsupported', (done) => {
-    const toCopy = 'xdd';
-    const clippy = new NeonClipboardSupport();
-
-    Object.defineProperty(navigator, 'clipboard', {
-      value: { writeText: jest.fn() },
-      writable: true,
-    });
-
-    Object.defineProperty(navigator, 'permissions', {
-      value: null,
-      writable: true,
-    });
-
-    setTimeout(() => {
-      expect(() => clippy.copyTo(toCopy)).rejects.toThrow();
       done();
     });
   });
